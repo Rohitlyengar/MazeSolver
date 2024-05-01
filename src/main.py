ae359d3b3d2d88173ea09cf0f13c226e1ccb2d65
Rohit Iyengar <rohitiyengar8@gmail.com>
2024-05-01 20:14:13 +0530

    Drew line

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-from graphics import Window
+from graphics import (Window, Point, Line)
 
 def main():
     win = Window(800, 600)
+
+    p1 = Point(100, 100)
+    p2 = Point(200, 100)
+
+    line1 = Line(p1, p2)
+    win.draw_line(line1, "red")
+
     win.wait_for_close()
 
 if __name__ == '__main__':
