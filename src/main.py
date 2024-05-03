6a9a2aec346322e697e8f2f26caeb42463278b0f
Rohit Iyengar <rohitiyengar8@gmail.com>
2024-05-03 18:38:13 +0530

    Created Maze Class

--- src/main.py
+++ src/main.py
@@ -1,16 +1,10 @@
 from graphics import (Window, Point, Line)
-from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
 
-    cell1 = Cell(win)
-    cell2 = Cell(win)
-
-    cell1.draw(50, 50, 100, 100)
-    cell2.draw(125, 125, 200, 200)
-
-    cell1.draw_move(cell2, True)
+    Maze(0, 0, 10, 10, 50, 50, win)
 
     win.wait_for_close()
 
